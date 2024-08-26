71143e9cfa6e3e9c5ac69090931e17e24e07761a
Dan Niemi <niemdani@gmail.com>
2024-08-26 15:27:11 -0500

    before finishorder

--- public/index.js
+++ public/index.js
@@ -7,29 +7,38 @@ function clickerGame() {
     playerList: {},
     state: 0, //0,1,2 for pre, during, and post game
     finishOrder: [],
+    startTime: null,
+    time:null,
+    
 
     init() {
+      this.time = Date.now()
       this.socket = io();
       this.socket.on("newConnection", (id, playerList) => {
         this.id = id;
         this.playerList = playerList;
       });
-      this.socket.on("setState", (state) => (this.state = state));
-      this.socket.on("updateScores", (playerList) => (this.playerList = playerList));
-      this.socket.on("gameStarted", (playerList, finishOrder) => {
-        this.state = 1;
-        this.playerList = playerList;
-        this.finishOrder = finishOrder;
-      });
-      this.socket.on("gameEnded", () => (this.state = 2));
-      this.socket.on("playerFinished", (finishOrder) => (this.finishOrder = finishOrder));
+      this.socket.on("stateUpdated", (state) => (this.state = state));
+      this.socket.on("playerListUpdated", (playerList) => (this.playerList = playerList));
+      this.socket.on("finishOrderUpdated",(finishOrder)=> this.finishOrder = finishOrder )
+      this.socket.on("countdownStarted", (dur) => this.startTime = Date.now() + dur )
+      setInterval(()=>{
+        this.time = Date.now()
+      },1000)
     },
-    get finished() {
-      return this.playerList[this.id]?.finished;
+    get player(){
+      return this.playerList[this.id]
     },
-    get numPlayers(){
-      return Object.values(this.playerList).length
+    get players(){
+      return Object.values(this.playerList)
     },
+    get countdownTime(){
+      if(this.startTime){
+        return Math.floor((this.startTime-this.time)/1000)
+      }
+      return false
+    },
+  
     submitName() {
       if (this.playerName.trim() !== "") {
         this.hasName = true;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { time } = require("console");
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -6,61 +7,77 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+
 let playerList = {};
-let state = 0; //0,1,2 for pre, during, and post game
+let standings = [];
+let state = 0; //0,1,2,3 for pre, countdown, during, and post game
 let finishOrder = [];
+let startTime = null;
 const CLICK_GOAL = 5;
+const COUNTDOWN_DURATION = 5000; //in ms
 
 io.on("connection", (socket) => {
-  socket.emit("newConnection", socket.id,playerList);
-  
-  if(!Object.keys(playerList).length){
-    console.log('set state')
-    state = 0
-  }
-  socket.emit("setState", state);
+  socket.emit("newConnection", socket.id, playerList);
 
+  if (!Object.keys(playerList).length) {
+    state = 0;
+  }
+  socket.emit("stateUpdated", state);
   socket.on("newPlayer", (name) => {
-    playerList[socket.id] = { name: name, count: 0, finished: false };
-    io.emit("updateScores", playerList);
+    playerList[socket.id] = { name: name, count: 0, finished: false, time: null };
+    io.emit("playerListUpdated", playerList);
   });
 
-  socket.on("startGame", () => {
-    if (state != 1) {
-      state = 1;
-      Object.values(playerList).forEach((obj) => {
-        obj.count = 0;
-        obj.finished = false;
-      });
-      finishOrder = [];
-      io.emit("gameStarted", playerList, finishOrder);
-    }
+  socket.on("resetGame", () => {
+    state = 0;
+    io.emit("stateUpdated", state);
+    Object.values(playerList).forEach((obj) => {
+      obj.count = 0;
+      obj.finished = false;
+    });
+    finishOrder = [];
+    io.emit("finishOrderUpdated", finishOrder);
+    io.emit("playerListUpdated", playerList);
+  });
+
+  socket.on("startCountdown", () => {
+    state = 1;
+    io.emit("stateUpdated", state);
+    io.emit("countdownStarted", COUNTDOWN_DURATION);
+    setTimeout(() => {
+      startTime = Date.now();
+      state = 2;
+      io.emit("stateUpdated", state);
+    }, COUNTDOWN_DURATION);
   });
 
   socket.on("click", () => {
-    if (playerList[socket.id] && state == 1) {
-      playerList[socket.id].count++;
+    
+    if (playerList[socket.id] && state == 2) {
+      let p = playerList[socket.id];
+      p.count++;
 
-      if (playerList[socket.id].count >= CLICK_GOAL) {
-        playerList[socket.id].finished = true;
-        finishOrder.push(playerList[socket.id].name);
-        io.emit("playerFinished", finishOrder);
+      if (p.count >= CLICK_GOAL) {
+        p.finished = true;
+        p.time = Date.now() - startTime;
+        finishOrder.push(p.name);
+        io.emit("finishOrderUpdated", finishOrder);
       }
-      io.emit("updateScores", playerList);
+      io.emit("playerListUpdated", playerList);
       checkEnd();
     }
   });
 
   socket.on("disconnect", () => {
     delete playerList[socket.id];
-    io.emit("updateScores", playerList);
+    io.emit("playerListUpdated", playerList);
     checkEnd();
   });
 
   function checkEnd() {
     if (Object.values(playerList).every((obj) => obj.count >= CLICK_GOAL)) {
-      state = 2;
-      io.emit("gameEnded");
+      state = 3;
+      io.emit("stateUpdated", state);
     }
   }
 });
